9d19facaaa7acb280f82115bca837f6150c3e97b
zhangyibo <goldeneyezhang@163.com>
2020-02-04 16:29:26 +0800

    perthis

--- src/main/java/com/yibo/aopdemo/example/MyAspect4.java
+++ src/main/java/com/yibo/aopdemo/example/MyAspect4.java
@@ -4,18 +4,20 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Aspect("perthis(this(com.yibo.aopdemo.example.Apple3))")
 @Scope("prototype")
+@Component
 public class MyAspect4 {
     public MyAspect4(){
-        System.out.println("create MyAspect instance, address: " + toString());
+        System.out.println("create MyAspect instance4, address: " + toString());
     }
     @Around("this(com.yibo.aopdemo.example.Apple3)")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
-        System.out.println("this is before around advice");
+        System.out.println("this is before around advice4");
         Object result = pjp.proceed();
-        System.out.println("this is after around advice");
+        System.out.println("this is after around advice4");
         return result;
     }
 }
--- src/main/java/com/yibo/aopdemo/example/MyAspect5.java
+++ src/main/java/com/yibo/aopdemo/example/MyAspect5.java
@@ -4,18 +4,20 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-@Aspect("pertarget(com.yibo.aopdemo.example.Apple3.eat())")
+@Aspect("pertarget(target(com.yibo.aopdemo.example.Fruit))")
 @Scope("prototype")
+@Component
 public class MyAspect5 {
     public MyAspect5(){
-        System.out.println("create MyAspect instance, address: " + toString());
+        System.out.println("create MyAspect5 instance, address: " + toString());
     }
-    @Around("target(com.yibo.aopdemo.example.Apple3.eat())")
+    @Around("target(com.yibo.aopdemo.example.Fruit)")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
-        System.out.println("this is before around advice");
+        System.out.println("this is before around advice 5");
         Object result = pjp.proceed();
-        System.out.println("this is after around advice");
+        System.out.println("this is after around advice 5");
         return result;
     }
 }
